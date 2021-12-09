5455673ec28b392e080c2982f50e6761ab31f2a6
SahandGN <sahand.gn@gmail.com>
2021-12-09 12:06:16 +0330

    jquery add

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
+$(document).ready(function (){
+    alert("Click on title of each section and hide the section")
+})
+
+$(document).ready(function () {
+    $("#france_flag_header").click(function () {
+        $("#france_flag").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#france_provinces_header").click(function () {
+        $("#france_provinces").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#important_provinces_header").click(function () {
+        $("#important_provinces").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#french_loanwords_header").click(function () {
+        $("#french_loanwords").slideToggle("slow");
+    });
+});
+
+$(document).ready(function () {
+    $("#history_container_header").click(function () {
+        $(".history_container").slideToggle("slow");
+    });
+});
