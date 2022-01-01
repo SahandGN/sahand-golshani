7455ff3b30dd24683c343febd0cfcb5014d969ea
SahandGN <sahand.gn@gmail.com>
2022-01-02 00:04:08 +0330

    some grammar text changes

--- index.php
+++ index.php
@@ -5,15 +5,26 @@ if (isset($_GET['submit_btn'])) {
 
     $input = $_GET['input'];
     echo "Result is: ";
+    try{
     calculate($input);
-
+    }
+    catch (DivisionByZeroError $e){
+        echo $e->getMessage();
+    }
+    catch (NullPointerException $e){
+        echo $e->getMessage();
+    }
 }
 
 
 function calculate($input)
 {
+    if ($input == null){
+        throw new NullPointerException('The $var does not exist');
+    }
     $result = eval("print (" . $input . ");");
     echo $result;
+
 }
 echo "<br>";
 echo "<button><a href='index.html'>  Back  </a></button>";
